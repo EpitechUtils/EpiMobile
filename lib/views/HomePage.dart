178c0ce2ba1d04d1047d4c869f40403351ae5d61
Cyril COLINET <cyril.colinet@epitech.eu>
2019-02-10 14:44:20 +0100

    Add default values

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -7,6 +7,7 @@ import 'package:mobile_intranet/components/ui/widgets/login_background.dart';
 import 'package:mobile_intranet/components/ui/widgets/profile_tile.dart';
 import 'package:mobile_intranet/components/utils/uidata.dart';
 import 'package:mobile_intranet/views/DashboardPage.dart';
+import 'package:mobile_intranet/views/LoginPage.dart';
 
 class HomePage extends StatefulWidget {
     HomePage({Key key, this.title}) : super(key: key);
@@ -133,16 +134,96 @@ class _HomePageState extends State<HomePage> {
         this._deviceSizes = MediaQuery.of(context).size;
 
         return Scaffold(
+            drawer: new Drawer(
+                child: ListView(
+                    children: <Widget>[
+                        // user drawer
+                        new UserAccountsDrawerHeader(
+                            accountName: Row(
+                                children: <Widget>[
+
+                                    // Fire
+                                    Container(
+                                        width: 10.0,
+                                        height: 10.0,
+                                        margin: EdgeInsets.only(right: 5),
+                                        decoration: new BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.orange
+                                        ),
+                                    ),
+
+                                    // Account name
+                                    Text('Cyril Colinet',
+                                        style: TextStyle(fontSize: 20)
+                                    )
+                                ],
+                            ),
+                            accountEmail: new Text('cyril.colinet@epitech.eu'),
+                            currentAccountPicture: new CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: new NetworkImage('https://avatars2.githubusercontent.com/u/8271271?s=400&u=aa8e1179b37cb3d76dc9d35b939862a3b40aa9e4&v=4')
+                            ),
+                            otherAccountsPictures: <Widget>[
+
+                                /// Credits
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                        Text("60", style: TextStyle(fontSize: 20, color: Colors.white)),
+                                        Text("Crédits", style: TextStyle(color: Colors.white)),
+                                    ],
+                                ),
+
+                                /// GPA
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                        Text("3.56", style: TextStyle(fontSize: 20, color: Colors.white)),
+                                        Text("GPA", style: TextStyle(color: Colors.white)),
+                                    ],
+                                ),
+
+                                // Spices
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                        Text("20", style: TextStyle(fontSize: 20, color: Colors.white)),
+                                        Text("Épices", style: TextStyle(color: Colors.white)),
+                                    ],
+                                )
+
+                            ],
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                image: DecorationImage(
+                                    colorFilter:
+                                        new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                                    image: AssetImage("assets/images/drawer-header.jpg"),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                        ),
+                        new ListTile(
+                            title: new Text('About Page'),
+                            onTap: () {
+                                Navigator.of(context).pop();
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (BuildContext context) => new LoginPage()));
+                            },
+                        ),
+                    ],
+                ),
+            ),
             body: Stack(
                 fit: StackFit.expand,
                 children: <Widget>[
-
                     // Background
                     LoginBackground(showIcon: false),
-
                     // Other cards
                     this.displayCards()
-
                 ],
             )
         );
