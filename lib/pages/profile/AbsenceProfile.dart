a28b16d5aaac62bd652b400b8c4196328a4ff202
Lucas GRAS <lucas.gras@face06.com>
2020-02-23 02:40:16 +0100

    [~] Update absence page

--- lib/pages/profile/AbsenceProfile.dart
+++ lib/pages/profile/AbsenceProfile.dart
@@ -13,35 +13,34 @@ class AbsenceProfile extends StatelessWidget {
             itemCount: this.profile.missed.length,
             itemBuilder: (BuildContext context, int index) {
                 return Container(
-                    child: Card(
-                        child: Column(
-                            children: <Widget>[
-                                Container(
-                                    child: Text(
-                                        this.profile.missed[index]["categ_title"].toString() + " le " + this.profile.missed[index]["begin"],
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w600
-                                        )
-                                    ),
-                                    margin: EdgeInsets.only(bottom: 10),
+                    child: Column(
+                        children: <Widget>[
+                            Container(
+                                child: Text(
+                                    this.profile.missed[index]["categ_title"].toString() + " le " + this.profile.missed[index]["begin"],
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600
+                                    )
                                 ),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                        Container(
-                                            alignment: Alignment.center,
-                                            child: Text(
-                                                (this.profile.missed[index]["acti_title"].toString().length > 50) ?
-                                                this.profile.missed[index]["acti_title"].toString().substring(0, 50) + "..." : this.profile.missed[index]["acti_title"],
-                                                style: TextStyle(
-                                                    fontStyle: FontStyle.italic
-                                                ),
+                                margin: EdgeInsets.only(bottom: 10),
+                            ),
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                    Container(
+                                        alignment: Alignment.center,
+                                        child: Text(
+                                            (this.profile.missed[index]["acti_title"].toString().length > 50) ?
+                                            this.profile.missed[index]["acti_title"].toString().substring(0, 50) + "..." : this.profile.missed[index]["acti_title"],
+                                            style: TextStyle(
+                                                fontStyle: FontStyle.italic
                                             ),
-                                        )
-                                    ],
-                                )
-                            ],
-                        ),
+                                        ),
+                                    )
+                                ],
+                            ),
+                            Divider()
+                        ],
                     ),
                 );
             }
