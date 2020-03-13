e11497574dc72204d8850c897abe574869876d36
Cyril Colinet <cyrilcolinet.pro@gmail.com>
2020-03-13 16:07:34 +0100

    Fix black probelems

--- lib/pages/schedule/ScheduleSessionInformation.dart
+++ lib/pages/schedule/ScheduleSessionInformation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_intranet/layouts/default.dart';
 import 'package:mobile_intranet/pages/schedule/sessions/ScheduleSessionNormal.dart';
 import 'package:mobile_intranet/parser/components/schedule/ScheduleSession.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,7 +8,8 @@ import 'package:mobile_intranet/pages/schedule/sessions/ScheduleSessionRdv.dart'
 class ScheduleSessionInformation extends StatefulWidget {
     ScheduleSession scheduleSession;
 
-    ScheduleSessionInformation({Key key, @required this.scheduleSession}) : super(key: key);
+    ScheduleSessionInformation({Key key, @required this.scheduleSession})
+        : super(key: key);
 
     @override
     State<StatefulWidget> createState() => _ScheduleSessionInformation();
@@ -16,27 +18,32 @@ class ScheduleSessionInformation extends StatefulWidget {
 class _ScheduleSessionInformation extends State<ScheduleSessionInformation> {
     SharedPreferences preferences;
 
-    _ScheduleSessionInformation() {;
-        SharedPreferences.getInstance().then((SharedPreferences prefs) => this.setState(() => this.preferences = prefs));
+    _ScheduleSessionInformation() {
+        SharedPreferences.getInstance().then((SharedPreferences prefs) =>
+            this.setState(() => this.preferences = prefs));
     }
 
     @override
-    Widget build(BuildContext context)
-    {
-	return Scaffold(
-	    appBar: AppBar(
-		title: Text(this.widget.scheduleSession.activityTitle),
-	    ),
-	    body: buildSessionInformationFromType(context),
-	);
+    Widget build(BuildContext context) {
+        return DefaultLayout(
+            title: this.widget.scheduleSession.activityTitle,
+            child: buildSessionInformationFromType(context),
+        );
     }
 
-    Widget buildSessionInformationFromType(BuildContext context)
-    {
-        if (this.preferences == null)
+    Widget buildSessionInformationFromType(BuildContext context) {
+        if (this.preferences == null) {
             return Center(child: CircularProgressIndicator());
-	if (this.widget.scheduleSession.typeCode == "rdv")
-		return ScheduleSessionRdv(scheduleSession: this.widget.scheduleSession, preferences: this.preferences);
-	return ScheduleSessionNormal(scheduleSession: this.widget.scheduleSession, preferences: this.preferences);
+        } else if (this.widget.scheduleSession.typeCode == "rdv") {
+            return ScheduleSessionRdv(
+                scheduleSession: this.widget.scheduleSession,
+                preferences: this.preferences
+            );
+        }
+
+        return ScheduleSessionNormal(
+            scheduleSession: this.widget.scheduleSession,
+            preferences: this.preferences
+        );
     }
 }
--- lib/pages/schedule/sessions/ScheduleSessionNormal.dart
+++ lib/pages/schedule/sessions/ScheduleSessionNormal.dart
@@ -1,154 +1,170 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_intranet/parser/components/schedule/ScheduleSession.dart';
-import 'package:mobile_intranet/parser/Parser.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:mobile_intranet/parser/components/subcomponents/registrationSlot/RegistrationSlots.dart';
-import 'package:mobile_intranet/parser/components/subcomponents/registrationSlot/RegistrationSlot.dart';
 import 'package:mobile_intranet/utils/network/IntranetAPIUtils.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ScheduleSessionNormal extends StatefulWidget {
     ScheduleSession scheduleSession;
     SharedPreferences preferences;
 
-    ScheduleSessionNormal({Key key, @required this.scheduleSession, @required this.preferences}) : super(key: key);
+    ScheduleSessionNormal(
+        {Key key, @required this.scheduleSession, @required this.preferences})
+        : super(key: key);
 
     @override
     State<StatefulWidget> createState() => _ScheduleSessionNormalState();
 }
 
 class _ScheduleSessionNormalState extends State<ScheduleSessionNormal> {
-
     Widget buildInformation(BuildContext context) {
         return Container(
-	    child: Column(
-		children: <Widget>[
-		    Container(
-			margin: EdgeInsets.only(top: 10, bottom: 10),
-			child: Container(
-			    child: Text("Informations", style: TextStyle(fontWeight: FontWeight.w600),),
-			)
-		    ),
-		    Row(
-			mainAxisAlignment: MainAxisAlignment.spaceAround,
-			children: <Widget>[
-			    Container(
-				child: Column(
-				    children: <Widget>[
-					Icon(
-					    Icons.people,
-					),
-					Text(this.widget.scheduleSession.numberStudentsRegistered.toString())
-				    ],
-				)
-			    ),
-			    Container(
-				child: Column(
-				    children: <Widget>[
-					Icon(
-					    Icons.hourglass_full,
-					),
-					Text(this.widget.scheduleSession.hoursAmount)
-				    ],
-				)
-			    ),
-			    Container(
-				child: Column(
-				    children: <Widget>[
-					Icon(
-					    Icons.location_on,
-					),
-					Text(this.widget.scheduleSession.room.code.substring(this.widget.scheduleSession.room.code.lastIndexOf('/') + 1, this.widget.scheduleSession.room.code.length))
-				    ],
-				)
-			    )
-			],
-		    )
-		],
-	    ),
-	);
+            child: Column(
+                children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Container(
+                            child: Text(
+                                "Informations",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                        )),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                            Container(
+                                child: Column(
+                                    children: <Widget>[
+                                        Icon(
+                                            Icons.people,
+                                        ),
+                                        Text(this
+                                            .widget
+                                            .scheduleSession
+                                            .numberStudentsRegistered
+                                            .toString())
+                                    ],
+                                )),
+                            Container(
+                                child: Column(
+                                    children: <Widget>[
+                                        Icon(
+                                            Icons.hourglass_full,
+                                        ),
+                                        Text(this.widget.scheduleSession.hoursAmount)
+                                    ],
+                                )),
+                            Container(
+                                child: Column(
+                                    children: <Widget>[
+                                        Icon(
+                                            Icons.location_on,
+                                        ),
+                                        Text(this.widget.scheduleSession.room.code.substring(
+                                            this.widget.scheduleSession.room.code.lastIndexOf('/') +
+                                                1,
+                                            this.widget.scheduleSession.room.code.length))
+                                    ],
+                                ))
+                        ],
+                    )
+                ],
+            ),
+        );
     }
 
     Widget buildProfessors(BuildContext context) {
         return Expanded(
-	    child: Column(
-		children: <Widget>[
-		    Container(
-			margin: EdgeInsets.only(top: 10),
-			child: Container(
-			    child: Text("Professeurs", style: TextStyle(fontWeight: FontWeight.w600),),
-			)
-		    ),
-		    Flexible(
-			child: ListView.builder(
-			    scrollDirection: Axis.horizontal,
-			    itemCount: this.widget.scheduleSession.professors.length,
-			    itemBuilder: (BuildContext context, int index) {
-				return Container(
-				    padding: EdgeInsets.only(left: 5),
-				    child: Align(
-					alignment: Alignment.topLeft,
-					child: Container(
-					    width: 50.0,
-					    height: 50.0,
-					    decoration: BoxDecoration(
-						shape: BoxShape.circle,
-						image: DecorationImage(
-						    fit: BoxFit.cover,
-						    image: NetworkImage(this.widget.preferences.getString("autolog_url") + "/file/userprofil/" + this.widget.scheduleSession.professors[index].login.split('@')[0] + ".bmp")
-						)
-					    )
-					)
-				    )
-				);
-			    }
-			),
-		    )
-		],
-	    ),
-	);
+            child: Column(
+                children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Container(
+                            child: Text(
+                                "Professeurs",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                        )),
+                    Flexible(
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: this.widget.scheduleSession.professors.length,
+                            itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                    padding: EdgeInsets.only(left: 5),
+                                    child: Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Container(
+                                            width: 50.0,
+                                            height: 50.0,
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                image: DecorationImage(
+                                                    fit: BoxFit.cover,
+                                                    image: NetworkImage(this
+                                                        .widget
+                                                        .preferences
+                                                        .getString("autolog_url") +
+                                                        "/file/userprofil/" +
+                                                        this
+                                                            .widget
+                                                            .scheduleSession
+                                                            .professors[index]
+                                                            .login
+                                                            .split('@')[0] +
+                                                        ".bmp"))))));
+                            }),
+                    )
+                ],
+            ),
+        );
     }
 
     @override
-    Widget build(BuildContext context)
-    {
-	return SafeArea(
-	    child: Column(
-		children: <Widget>[
-		    Container(
-			height: 200,
-			child: Card(
-			    child: Column(
-				children: <Widget>[
-				    buildInformation(context),
-				    Divider(),
-				    buildProfessors(context)
-				],
-			    )
-			)
-		    ),
-		    Container(
-			child: RaisedButton(
-			    onPressed: () {
-				SharedPreferences.getInstance().then((SharedPreferences prefs) {
-					IntranetAPIUtils().registerToActivity(
-					    prefs.getString("autolog_url"),
-					    ((this.widget.scheduleSession.scolarYear == null) ? DateTime.now().year.toString() : this.widget.scheduleSession.scolarYear),
-					    this.widget.scheduleSession.codeModule,
-					    this.widget.scheduleSession.codeInstance,
-					    this.widget.scheduleSession.codeActivity,
-					    this.widget.scheduleSession.eventRegistered is bool
-					).then((dynamic res) {
-					    this.setState(() {});
-					});
-				});
-			    },
-			    color: (this.widget.scheduleSession.eventRegistered is bool ? Colors.green : Colors.red),
-			    child: Text((this.widget.scheduleSession.eventRegistered is bool ? "S'inscrire" : "X"), style: TextStyle(color: Colors.white),),
-			),
-		    )
-		],
-	    )
-	);
+    Widget build(BuildContext context) {
+        return SafeArea(
+            child: Column(
+                children: <Widget>[
+                    Container(
+                        height: 200,
+                        child: Card(
+                            child: Column(
+                                children: <Widget>[
+                                    buildInformation(context),
+                                    Divider(),
+                                    buildProfessors(context)
+                                ],
+                            ))),
+                    Container(
+                        child: RaisedButton(
+                            onPressed: () {
+                                SharedPreferences.getInstance().then((SharedPreferences prefs) {
+                                    IntranetAPIUtils()
+                                        .registerToActivity(
+                                        prefs.getString("autolog_url"),
+                                        ((this.widget.scheduleSession.scolarYear == null)
+                                            ? DateTime.now().year.toString()
+                                            : this.widget.scheduleSession.scolarYear),
+                                        this.widget.scheduleSession.codeModule,
+                                        this.widget.scheduleSession.codeInstance,
+                                        this.widget.scheduleSession.codeActivity,
+                                        this.widget.scheduleSession.eventRegistered is bool)
+                                        .then((dynamic res) {
+                                        this.setState(() {});
+                                    });
+                                });
+                            },
+                            color: (this.widget.scheduleSession.eventRegistered is bool
+                                ? Colors.green
+                                : Colors.red),
+                            child: Text(
+                                (this.widget.scheduleSession.eventRegistered is bool
+                                    ? "S'inscrire"
+                                    : "X"),
+                                style: TextStyle(color: Colors.white),
+                            ),
+                        ),
+                    )
+                ],
+            ));
     }
 }
